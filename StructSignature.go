fdf8e68231452ed12e0076a97c600ee6ffa36411
Johnzeng <Johnz@misfit.com>
2016-08-17 15:32:03 +0800

    get signature from struct define

---
+++ StructSignature.go
@@ -0,0 +1,39 @@
+package StructSignature
+
+import (
+	"crypto/md5"
+	"fmt"
+	"reflect"
+)
+
+func GetSignature(i interface{}) string {
+	refleType := reflect.TypeOf(i)
+	searchResult := searchInterface(refleType)
+	data := []byte(searchResult)
+	return fmt.Sprintf("%x", md5.Sum(data))
+}
+
+func searchInterface(i reflect.Type) string {
+	switch i.Kind() {
+	case reflect.Func:
+		return ""
+	case reflect.Ptr:
+		return fmt.Sprintf("%d:%s", i.Kind(), searchInterface(i.Elem()))
+	case reflect.Struct:
+		ret := ""
+		for index := 0; index < i.NumField(); index++ {
+			field := i.Field(index)
+			ret += fmt.Sprintf("%s:%s", field.Name, searchInterface(field.Type))
+		}
+		return fmt.Sprintf("%s:%s", i.String(), ret)
+	case reflect.Map:
+		keyType := searchInterface(i.Key())
+		valueType := searchInterface(i.Elem())
+		return fmt.Sprintf("%s_%s", keyType, valueType)
+	case reflect.Slice, reflect.Array:
+		valueType := searchInterface(i.Elem())
+		return fmt.Sprintf("%d:%s", i.Kind(), valueType)
+	default:
+		return fmt.Sprintf("%d", i.Kind())
+	}
+}
---
+++ StructSignature_test.go
@@ -0,0 +1,50 @@
+package StructSignature
+
+import "testing"
+import "reflect"
+
+type TestStruct struct {
+	i  int
+	s  string
+	f  float32
+	ff float64
+}
+
+type EmbbedStruct struct {
+	t TestStruct
+	i int
+	s []int
+	m map[int]string
+}
+
+func TestInterfaceName(t *testing.T) {
+	ret := searchInterface(reflect.TypeOf(TestStruct{}))
+	if "StructSignature.TestStruct:i:2s:24f:13ff:14" != ret {
+		t.Errorf("wrong signature string %s", ret)
+	}
+	t.Logf("%s", ret)
+}
+
+func TestGetSignature(t *testing.T) {
+	ret := GetSignature(TestStruct{})
+	if "19e61b60e670dfe0f5913ac4c5875590" != ret {
+		t.Errorf("Wrong signature %s", ret)
+	}
+	t.Logf("%s", ret)
+}
+
+func TestComplicateStruct(t *testing.T) {
+	ret := searchInterface(reflect.TypeOf(EmbbedStruct{}))
+	if "StructSignature.EmbbedStruct:t:StructSignature.TestStruct:i:2s:24f:13ff:14i:2s:23:2m:2_24" != ret {
+		t.Errorf("wrong signature ret %s", ret)
+	}
+	t.Logf("%s", ret)
+}
+
+func TestComplicateSignature(t *testing.T) {
+	ret := GetSignature(EmbbedStruct{})
+	if "9c2943a7d6d4f8d64128698493d4e99a" != ret {
+		t.Errorf("Wrong signature %s", ret)
+	}
+	t.Logf("%s", ret)
+}
